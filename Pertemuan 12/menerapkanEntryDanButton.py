4b0821303e6157259080aef1291f1ab5d1d315ca
Muhammad Luthfi Pratama Putra <86417070+Piluthfi@users.noreply.github.com>
2024-05-30 08:09:43 +0700

    Create menerapkanEntryDanButton.py

---
+++ Pertemuan 12/menerapkanEntryDanButton.py
@@ -0,0 +1,20 @@
+#CREATING GUI USING TKINTER (2)
+from tkinter import *
+#Jalankan code untuk tiap topik bergantian. Beri tanda #
+#untuk topik yang #tidak dijalankan.
+
+print('=====================Exercise#1====================')
+print('===Mencetak data dari Entry Widget dengan Button===')
+print('===================================================')
+root = Tk()
+root.geometry('400x200')
+
+def CetakData():
+    teks = entry1.get()
+    print(teks)
+    return None
+
+entry1 = Entry(root, width = 20); entry1.pack()
+Button(root, text="Cetak Data", command=CetakData).pack()
+
+root.mainloop()
